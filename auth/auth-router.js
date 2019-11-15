441d88e3888c5862e67e7c906052520257db5aec
Brenda Joshua <brendajosh.bj@gmail.com>
2019-11-15 12:34:33 +0300

    added register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../auth/auth-model');
+const generateToken = require('./generateToken');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+  .then(saved => {
+    res.status(201).json(saved);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'failed to create user', err });
+  });
 });
 
 router.post('/login', (req, res) => {
